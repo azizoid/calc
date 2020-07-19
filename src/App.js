95702f9f3e0e5cc855d05cd9642aee42708d7acd
Aziz <shaqhuseynov@gmail.com>
2020-07-19 08:11:25 +0200

    Localization
    added localization
    optimized click actions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import parseParams from "./assets/parse.params.js";
 
 // import Navbar from "./components/navbar/navbar.component";
 
+import Localization from "./localization/Localization";
+
 // import "./App.scss";
 
 const App = () => {
@@ -26,12 +28,19 @@ const App = () => {
   const [form, setForm] = useState({ x1: 1, x2: 1, y1: 1, y2: 1 });
   const [message, setMessage] = useState("");
   const [alert, setAlert] = useState(0);
+  const [lang, setLang] = useState("en");
 
   useEffect(() => {
     const url = new URL(window.location);
     let params = parseParams(url.search);
     if (Object.keys(params).length > 0) {
-      const custom = _.hasIn(params, "custom") ? params.custom.split(":") : {};
+      // const custom = _.hasIn(params, "custom") ? params.custom.split(":") : {};
+
+      if (_.hasIn(params, "lang")) {
+        if (params.lang in Localization) {
+          setLang(params.lang);
+        } else setLang("en");
+      }
 
       if (
         _.hasIn(params, "custom_base") &&
@@ -71,7 +80,6 @@ const App = () => {
       currList.forEach((arr) => tmp.push(arr.base, arr.target));
       setSymbols(_.uniq(tmp));
 
-      console.log(currList);
       setForm({
         x1: 1,
         x2: currList[0].rate,
@@ -84,178 +92,183 @@ const App = () => {
 
   useEffect(() => {
     if (!alert) return setMessage("");
+    setMessage(Localization[lang].message);
     const timer = setTimeout(() => {
-      setMessage("We dont have this currency yet");
       setAlert(0);
     }, 1500);
     return () => {
       clearTimeout(timer);
     };
-  }, [alert]);
+  }, [alert, lang]);
 
-  const onHandlerChangex1 = (event) => {
-    let { value } = event.target;
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
+
+  const onHandlerChangeX = (event) => {
+    let { name, value } = event.target;
     value = value > 0 ? value * 1 : 0;
 
-    const code = form.y1 + form.y2;
-    const currency = currList && currList.filter((arr) => arr.code === code);
+    const code = name === "x1" ? form.y1 + form.y2 : form.y2 + form.y1;
+    const currency = currList.filter((arr) => arr.code === code);
+
     const rate = currency[0].rate;
 
-    const params = { x1: value, x2: (value * rate).toFixed(4) };
-
+    let params = {};
+    if (name === "x1") {
+      params = { x1: value, x2: (value * rate).toFixed(4) };
+    } else if (name === "x2") {
+      params = { x1: (value * rate).toFixed(4), x2: value };
+    }
     setForm((prev) => ({ ...prev, ...params }));
   };
 
-  const onHandlerChangex2 = (event) => {
-    let { value } = event.target;
-    value = value > 0 ? value * 1 : 0;
+  const onHandlerChangeY = (event) => {
+    let { name, value } = event.target;
 
-    const code = form.y2 + form.y1;
+    if (name === "y1") {
+      if (value === form.y2) return;
 
-    if (currList) {
-      const currency = currList.filter((arr) => arr.code === code);
-      const rate = currency[0].rate;
+      const code = value + form.y2;
+      const currency = _.find(currList, { code: code });
 
-      const params = { x1: (value * rate).toFixed(4), x2: value };
+      if (!currency) {
+        setAlert(1);
+        return;
+      } else setMessage("");
 
-      setForm((prev) => ({ ...prev, ...params }));
+      setForm((prev) => ({
+        ...prev,
+        x1: (currency.rate * prev.x2).toFixed(4),
+        y1: value,
+      }));
+    } else if (name === "y2") {
+      if (value === form.y1) return;
+
+      const code = form.y1 + value;
+      const currency = _.find(currList, { code: code });
+
+      if (!currency) {
+        setAlert(1);
+        return;
+      } else setMessage("");
+
+      setForm((prev) => ({
+        ...prev,
+        x2: (currency.rate * prev.x1).toFixed(4),
+        y2: value,
+      }));
     }
-  };
-
-  const onHandlerChangey1 = (event) => {
-    let { value } = event.target;
-
-    if (value === form.y1) return;
-
-    const code = value + form.y2;
-    const currency = _.find(currList, { code: code });
-    if (!currency) {
-      setMessage("We dont have this currency yet");
-      setAlert(1);
-      return;
-    }
-    setMessage("");
-
-    let rate = currency.rate;
-
-    setForm((prev) => ({ ...prev, x1: rate, y1: value }));
-  };
-
-  const onHandlerChangey2 = (event) => {
-    let { value } = event.target;
-
-    if (value === form.y2) return;
-
-    const code = value + form.y1;
-    const currency = _.find(currList, { code: code });
-
-    if (!currency) {
-      setMessage("We dont have this currency yet");
-      setAlert(1);
-      return;
-    }
-    setMessage("");
-
-    let rate = currency.rate;
-
-    console.log(rate);
-
-    setForm((prev) => ({ ...prev, x2: rate, y2: value }));
   };
 
   return (
-    <div className="App container ">
-      <div className="row">
-        {message.length > 0 && (
-          <small className="col-12 alert alert-danger text-center">
-            {message}
-          </small>
-        )}
-        <form className="col-12">
-          <div className="form-row">
-            <div className="form-group col-4">Base:</div>
-            <div className="form-group col-4">
-              <input
-                type="number"
-                placeholder="1"
-                min="0"
-                value={form.x1}
-                name="x1"
-                className="form-control-sm w-100"
-                onChange={(e) => onHandlerChangex1(e)}
-              />
-            </div>
-            <div className="form-group col-4">
-              <select
-                className="form-control-sm"
-                value={form.y1}
-                name="y1"
-                onChange={(e) => onHandlerChangey1(e)}
-              >
-                {symbols &&
-                  symbols.map((value, index) => (
-                    <option value={value} key={index}>
-                      {value}
-                    </option>
-                  ))}
-              </select>
-            </div>
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          {message.length > 0 && (
+            <small className="col-12 alert alert-danger text-center">
+              {message}
+            </small>
+          )}
+          <div className="col-12 text-center badge badge-info">
+            {form.x1 +
+              " " +
+              form.y1 +
+              " " +
+              Localization[lang].equals +
+              "" +
+              form.x2 +
+              " " +
+              form.y2}
           </div>
-          <div className="form-row">
-            <div className="form-group col-4">Equals:</div>
-            <div className="form-group col-4">
-              <input
-                type="number"
-                placeholder="1"
-                min="0"
-                value={form.x2}
-                name="x2"
-                className="form-control-sm w-100"
-                onChange={(e) => onHandlerChangex2(e)}
-              />
-            </div>
-            <div className="form-group col-4">
-              <select
-                className="form-control-sm"
-                value={form.y2}
-                name="y2"
-                onChange={(e) => onHandlerChangey2(e)}
-              >
-                {symbols &&
-                  symbols.map((value, index) => (
-                    <option value={value} key={index}>
-                      {value}
-                    </option>
-                  ))}
-              </select>
-            </div>
-          </div>
-        </form>
-
-        <table className="table table-hover table-sm col-12">
-          <tbody>
-            {currList &&
-              currList.map(({ base, rate, target }, i) => (
-                <tr
-                  key={i}
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    setForm((prev) => ({
-                      ...prev,
-                      x1: 1,
-                      x2: rate,
-                      y1: base,
-                      y2: target,
-                    }));
-                  }}
+          <form className="col-12">
+            <div className="form-row">
+              <div className="form-group col-4">{Localization[lang].base}:</div>
+              <div className="form-group col-4">
+                <input
+                  type="number"
+                  placeholder="1"
+                  min="0"
+                  value={form.x1}
+                  name="x1"
+                  className="form-control-sm w-100"
+                  onChange={(e) => onHandlerChangeX(e)}
+                />
+              </div>
+              <div className="form-group col-4">
+                <select
+                  className="form-control-sm"
+                  value={form.y1}
+                  name="y1"
+                  onChange={(e) => onHandlerChangeY(e)}
                 >
-                  <td>1 {base}</td>
-                  <td>equals</td>
-                  <td> {rate + " " + target}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+                  {symbols &&
+                    symbols.map((value, index) => (
+                      <option value={value} key={index}>
+                        {value}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-4">
+                {Localization[lang].equals}:
+              </div>
+              <div className="form-group col-4">
+                <input
+                  type="number"
+                  placeholder="1"
+                  min="0"
+                  value={form.x2}
+                  name="x2"
+                  className="form-control-sm w-100"
+                  onChange={(e) => onHandlerChangeX(e)}
+                />
+              </div>
+              <div className="form-group col-4">
+                <select
+                  className="form-control-sm"
+                  value={form.y2}
+                  name="y2"
+                  onChange={(e) => onHandlerChangeY(e)}
+                >
+                  {symbols &&
+                    symbols.map((value, index) => (
+                      <option value={value} key={index}>
+                        {value}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
+          </form>
+
+          <table className="table table-hover table-sm col-12">
+            <tbody>
+              {currList &&
+                currList.map(({ base, rate, target }, i) => (
+                  <tr
+                    key={i}
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      setForm((prev) => ({
+                        ...prev,
+                        x1: 1,
+                        x2: rate,
+                        y1: base,
+                        y2: target,
+                      }));
+                    }}
+                  >
+                    <td>1 {base}</td>
+                    <td>{Localization[lang].equals}</td>
+                    <td> {rate + " " + target}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
---
+++ src/localization/Localization.js
@@ -0,0 +1,22 @@
+const Localization = {
+  en: {
+    equals: "equals",
+    base: "Base",
+    target: "Target",
+    message: "We dont have this currency yet",
+  },
+  ru: {
+    equals: "Равно",
+    base: "Основная",
+    target: "Конвертация",
+    message: "Эта валюта пока недоступна",
+  },
+  rs: {
+    equals: "jednako",
+    base: "Glavnica",
+    target: "Prevodnica",
+    message: "Ova valuta ne postoji jos",
+  },
+};
+
+export default Localization;
