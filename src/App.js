ba5ade84ce7696d31d25c38350e9fd22350641ca
Aziz <shaqhuseynov@gmail.com>
2020-07-23 06:29:11 +0200

    Switch button changed from text to SVG

--- src/App.js
+++ src/App.js
@@ -239,6 +239,7 @@ const App = () => {
                   type="number"
                   placeholder="1"
                   min="0"
+                  step="0.01"
                   value={form.x1}
                   name="x1"
                   className="form-control-sm w-100"
@@ -260,7 +261,9 @@ const App = () => {
                     ))}
                 </select>
                 <button
-                  onClick={() => {
+                  class="btn btn-link"
+                  onClick={(e) => {
+                    e.preventDefault();
                     setForm((prev) => ({
                       x1: prev.x2,
                       x2: prev.x1,
@@ -269,7 +272,23 @@ const App = () => {
                     }));
                   }}
                 >
-                  Switch
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-arrow-repeat"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2.854 7.146a.5.5 0 0 0-.708 0l-2 2a.5.5 0 1 0 .708.708L2.5 8.207l1.646 1.647a.5.5 0 0 0 .708-.708l-2-2zm13-1a.5.5 0 0 0-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 0 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0 0-.708z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 3a4.995 4.995 0 0 0-4.192 2.273.5.5 0 0 1-.837-.546A6 6 0 0 1 14 8a.5.5 0 0 1-1.001 0 5 5 0 0 0-5-5zM2.5 7.5A.5.5 0 0 1 3 8a5 5 0 0 0 9.192 2.727.5.5 0 1 1 .837.546A6 6 0 0 1 2 8a.5.5 0 0 1 .501-.5z"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
@@ -282,6 +301,7 @@ const App = () => {
                   type="number"
                   placeholder="1"
                   min="0"
+                  step="0.01"
                   value={form.x2}
                   name="x2"
                   className="form-control-sm w-100"
