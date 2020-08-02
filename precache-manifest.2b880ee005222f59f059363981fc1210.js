ae5e572bea68669dd9d6ac8cc31d3c49cc9dcd50
Aziz <shaqhuseynov@gmail.com>
2020-08-02 06:13:00 +0200

    Updates

---
+++ precache-manifest.2b880ee005222f59f059363981fc1210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24db48252a07ab8b621fc458373c5d03",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "31fd16621b1dd74c7a34",
+    "url": "/currency/static/css/main.1c5ed388.chunk.css"
+  },
+  {
+    "revision": "c5a8108aeb0436847f11",
+    "url": "/currency/static/js/2.1c753efa.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/currency/static/js/2.1c753efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fd16621b1dd74c7a34",
+    "url": "/currency/static/js/main.88739fb8.chunk.js"
+  },
+  {
+    "revision": "280d1c3052d33c39d203",
+    "url": "/currency/static/js/runtime-main.7fed406f.js"
+  }
+]);
